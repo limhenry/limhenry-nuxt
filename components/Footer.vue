fa18ad10b9d90c860a76c45577b7eeed62dd0c34
Henry Lim <4544770+limhenry@users.noreply.github.com>
2022-11-19 00:02:53 +0100

    add: mastodon

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,6 +2,14 @@
   <footer class="grid gap-6 px-3 pt-24 pb-12 text-gray-500 dark:text-gray-400">
     <div class="grid grid-cols-4 sm:grid-cols-8 gap-y-4 gap-6 md:gap-8 justify-center justify-items-center mx-auto">
       <a
+        href="https://mastodon.social/@henrylim96"
+        target="_blank"
+        rel="me noopener noreferrer"
+        class="transition hover:text-gray-600 dark:hover:text-gray-300"
+      >
+        <IconMastodon class="w-[22px] h-[22px]" />
+      </a>
+      <a
         href="https://twitter.com/henrylim96"
         target="_blank"
         rel="noopener noreferrer"
@@ -48,14 +56,6 @@
         class="transition hover:text-gray-600 dark:hover:text-gray-300"
       >
         <IconGde class="w-[22px] h-[22px]" />
-      </a>
-      <a
-        href="https://www.patreon.com/remoteforslides/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="transition hover:text-gray-600 dark:hover:text-gray-300"
-      >
-        <IconKofi class="w-[22px] h-[22px]" />
       </a>
       <a
         href="https://patreon.com/remoteforslides"
